52b5aa246e9affb6e8c3592b5e0a1c1639cd48b8
aryansapsod <sapsodaryan@gmail.com>
2026-01-11 17:49:48 +0530

    Creating venv

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -27,13 +27,23 @@ def insert_new_product(connection, product):
     connection.commit()
     return cursor.lastrowid
 
+def delete_product_by_id(connection, product_id):
+    cursor = connection.cursor()
+    query = ("DELETE FROM products WHERE product_id =" + str(product_id))
+    # data = (product_id, )
+    cursor.execute(query)
+    connection.commit()
+    # return cursor.rowcount
+
 
 if __name__ == "__main__":
     connection = get_sql_connection()
-    print(get_all_products(connection))
-    print(insert_new_product(connection, {
-        "product_id": 10,
-        "product_name": "Onion",
-        "uom_id": 2,
-        "price_per_unit": 20
-    }))
+    # print(get_all_products(connection))
+    # print(insert_new_product(connection, {
+    #     "product_id": 10,
+    #     "product_name": "Onion",
+    #     "uom_id": 2,
+    #     "price_per_unit": 20
+    # }))
+    print(delete_product_by_id(connection, 4))
+    
